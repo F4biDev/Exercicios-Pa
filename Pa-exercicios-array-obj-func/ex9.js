097359ce3bff0e9fdf15e08a4de994b1723f3de1
F4bii_dev <fabianadossantosm88@gmail.com>
2026-06-10 19:22:50 -0300

    Add files via upload

--- Pa-exercicios-array-obj-func/ex9.js
+++ Pa-exercicios-array-obj-func/ex9.js
@@ -5,4 +5,42 @@
 //realizar saque (somente se houver saldo)
 //No saque, exiba a quantidade de cédulas entregues usando as notas: 100, 50, 20 e 10, utilizando o menor número possível de cédulas
 
+const conta = {
+    agencia: "0001",
+    numero: "456789",
+    senha:"5008",
+    saldo:"1000",
+    historico:[]
+};
 
+function validarAcesso(agencia, numero, senha){
+    if (agencia===conta.agencia &&
+        numero===conta.numero &&
+        senha===conta.senha
+
+    )
+    {
+        return true;
+
+    } else{
+        return false;
+    }
+}
+
+function exibirSaldo(){
+    console.log("/n0 valor deve ser multiplo de 10");
+    return;
+}
+
+function realizarSaque(valor){
+if (valor > conta.saldo){
+    console.log("/nsaldo insuficiente");
+    return;
+}
+if(valor % 10 !== 0){ console.log("/n0 valor deve ser multiplo de 10");
+    return;
+}   
+}
+conta.saldo -= valor;
+let restante=valor;
+let notas 100 =  
